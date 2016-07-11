ec5c9498384c6ed8f0d538f60f818d65267e3fce
axmo999 <axmo999@yahoo.co.jp>
2016-07-11 19:01:52 +0900

    face add
    顔写真追加　60％

---
+++ CramSchoolManagement/Controllers/students_faceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CramSchoolManagement.Models;
+
+namespace CramSchoolManagement.Controllers
+{
+    public class students_faceController : Controller
+    {
+        private Students_mModel db = new Students_mModel();
+        // GET: students_face
+        public ActionResult Index(long id)
+        {
+            var fileToRetrieve = db.students_face.Find(id);
+            return File(fileToRetrieve.Content, fileToRetrieve.ContentType);
+        }
+    }
+}
